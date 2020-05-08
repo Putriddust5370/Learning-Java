a967242899c58c4450c7a83fa28df6de5420762a
JOS PAUL <64735183+jospaul223@users.noreply.github.com>
2020-05-08 23:29:33 +0530

    can you please make this work?
    actually, what is the use of return and how can i implement in this code the same? please rewrite and explain in side .....

---
+++ Class11.java
@@ -0,0 +1,13 @@
+public class Class11
+	{
+		public static void main(String[] args)
+			{
+			abc( 2 , 3 );	
+			}
+		public static double abc(double x , double y)
+			{
+				double xy = x+y;
+				return xy;
+				
+			}
+	}
